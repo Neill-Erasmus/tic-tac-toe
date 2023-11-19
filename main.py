e1f76131ffe4808e47b14a19d594945b4d7c9f3c
Neill Erasmus <neill.erasmus20@gmail.com>
2023-11-19 20:27:32 +0200

    Added functionality to the game.

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
-import  board, player
+import  board, player, sys
 
 Board      = board.Board()
 Player_One = player.Player(1)
 Player_Two = player.Player(2)
 
-def GetUserInput(player : player.Player) -> tuple[int, int]:
-    row    = int(input(f"Player One: Which row would you like to play?: "))
-    column = int(input(f"Player One: Which column would you like to play?: "))
-    return (row, column)
+def GetRow(player : player.Player) -> int:
+    return int(input(f"{player.GetName()}: Which row would you like to play?: "))
+
+def GetColumn(player : player.Player) -> int:
+    return int(input(f"{player.GetName()}: Which column would you like to play?: "))
+
+def EndRound(player : player.Player) -> None:
+    print(f"{player.GetName()} won!")
+    if input("Would you like to play again? (Y/n): ").lower() == 'y':
+        Board.ResetBoard()
+    else:
+        input("Press any key to exit the application...")
+        sys.exit(0)
 
 def main():
     while True:
-        pass
-        
+        Board.UpdateBoard(player=Player_One, row=GetRow(player=Player_One), column=GetColumn(player=Player_One))
+        if Board.CheckWin(player=Player_One):
+            EndRound(player=Player_One)
+        Board.UpdateBoard(player=Player_Two, row=GetRow(player=Player_Two), column=GetColumn(player=Player_Two))
+        if Board.CheckWin(player=Player_Two):
+            EndRound(player=Player_Two)
 
 if __name__ == "__main__":
     main()
